9b79f1c6598824c5788460826e568c2ffe700f04
Navid Pour <navid.kouchakipour@utoronto.ca>
2023-01-23 20:11:19 -0500

    initial server code

--- src/app_kvServer/KVServer.java
+++ src/app_kvServer/KVServer.java
@@ -1,6 +1,18 @@
 package app_kvServer;
 
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+import logger.LogSetup;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 public class KVServer implements IKVServer {
+
+	private static Logger logger = Logger.getRootLogger();
 	/**
 	 * Start KV Server at given port
 	 * @param port given port for storage server to operate
@@ -11,79 +23,146 @@ public class KVServer implements IKVServer {
 	 *           currently not contained in the cache. Options are "FIFO", "LRU",
 	 *           and "LFU".
 	 */
+
+	 
+	// private static Logger logger = Logger.getRootLogger();
+
+	private int port;
+	private String strategy;
+	private boolean running;
+	private int cacheSize;
+	private ServerSocket serverSocket;
+
+
 	public KVServer(int port, int cacheSize, String strategy) {
-		// TODO Auto-generated method stub
+		this.port = port;
+		this.cacheSize = cacheSize;
+		this.strategy = strategy;
+		this.running = false;
 	}
 	
-	@Override
 	public int getPort(){
 		// TODO Auto-generated method stub
 		return -1;
 	}
 
-	@Override
     public String getHostname(){
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
     public CacheStrategy getCacheStrategy(){
+		return CacheStrategy.None;
 		// TODO Auto-generated method stub
-		return IKVServer.CacheStrategy.None;
+		// return strategy;
 	}
 
-	@Override
     public int getCacheSize(){
 		// TODO Auto-generated method stub
 		return -1;
 	}
 
-	@Override
     public boolean inStorage(String key){
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
     public boolean inCache(String key){
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
     public String getKV(String key) throws Exception{
 		// TODO Auto-generated method stub
 		return "";
 	}
 
-	@Override
     public void putKV(String key, String value) throws Exception{
 		// TODO Auto-generated method stub
 	}
 
-	@Override
     public void clearCache(){
 		// TODO Auto-generated method stub
 	}
 
-	@Override
     public void clearStorage(){
 		// TODO Auto-generated method stub
 	}
 
-	@Override
     public void run(){
-		// TODO Auto-generated method stub
+		initializeServer();
+
+		if(serverSocket != null) {
+	        while(isRunning()){
+	            try {
+	                Socket client = serverSocket.accept();                
+	                // ClientConnection connection = 
+	                // 		new ClientConnection(client);
+	                // new Thread(connection).start();
+	                
+	                // logger.info("Connected to " 
+	                // 		+ client.getInetAddress().getHostName() 
+	                // 		+  " on port " + client.getPort());
+	            } catch (IOException e) {
+	            	logger.error("Error! " +
+	            			"Unable to establish connection. \n", e);
+	            }
+	        }
+        }
+        // logger.info("Server stopped.");
 	}
 
-	@Override
     public void kill(){
 		// TODO Auto-generated method stub
 	}
 
-	@Override
     public void close(){
 		// TODO Auto-generated method stub
 	}
+
+	    
+    private boolean isRunning() {
+        return this.running;
+    }
+
+	private boolean initializeServer() {
+    	// logger.info("Initialize server ...");
+    	try {
+            serverSocket = new ServerSocket(port);
+            // logger.info("Server listening on port: " 
+            // 		+ serverSocket.getLocalPort());    
+            return true;
+        
+        } catch (IOException e) {
+        	// logger.error("Error! Cannot open server socket:");
+            if(e instanceof BindException){
+            	// logger.error("Port " + port + " is already bound!");
+            }
+            return false;
+        }
+	}
+	
+	public static void main(String[] args) {
+    	try {
+			// new LogSetup("logs/server.log", Level.ALL);
+			if(args.length != 1) {
+				System.out.println("Error! Invalid number of arguments!");
+				System.out.println("Usage: Server <port>!");
+			} else {
+				int port = Integer.parseInt(args[0]);
+				// new KVServer(port, 10, CacheStrategy.None).start();
+				new KVServer(port, 10, "None").run();
+			}
+		} 
+		// catch (IOException e) {
+		// 	System.out.println("Error! Unable to initialize logger!");
+		// 	e.printStackTrace();
+		// 	System.exit(1);
+		// } 
+		catch (NumberFormatException nfe) {
+			System.out.println("Error! Invalid argument <port>! Not a number!");
+			System.out.println("Usage: Server <port>!");
+			System.exit(1);
+		}
+    }
 }
