e3b28c552e1e5a4d451ccd90d95b70eae939f446
Shashwat Panwar <shashwat.panwar@mail.utoronto.ca>
2023-02-04 19:12:36 -0500

    Added string parsing methods for key, value, status in KVmessage interface

--- src/shared/messages/KVMessage.java
+++ src/shared/messages/KVMessage.java
@@ -14,24 +14,48 @@ public interface KVMessage {
 		DELETE_ERROR 	/* Delete - request successful */
 	}
 
+
 	/**
 	 * @return the key that is associated with this message, 
 	 * 		null if not key is associated.
 	 */
-	public String getKey();
+	// public String getKey();
+	public default String getKey(String message) {
+		String[] parts = message.split(":");
+		if (parts.length < 2) {
+			return null;
+		}
+		return parts[1];
+	}
 	
 	/**
 	 * @return the value that is associated with this message, 
 	 * 		null if not value is associated.
 	 */
-	public String getValue();
+	// public String getValue();
+	public default String getValue(String message) {
+		String[] parts = message.split(":");
+		if (parts.length < 3) {
+			return null;
+		}
+		return parts[2];
+	}
 	
 	/**
 	 * @return a status string that is used to identify request types, 
 	 * response types and error types associated to the message.
 	 */
-	public StatusType getStatus();
+	// public StatusType getStatus();
+	public default StatusType getStatus(String message) {
+		String[] parts = message.split(":");
+		if (parts.length < 1) {
+			return null;
+		}
+		try {
+			return StatusType.valueOf(parts[0]);
+		} catch (IllegalArgumentException e) {
+			return null;
+		}
+	}
 	
 }
-
-
