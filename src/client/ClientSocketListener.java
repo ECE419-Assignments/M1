72580eedf2a8295219778bdb236716a33e1b3d77
Navid Pour <navid.pour@outlook.com>
2023-02-08 15:56:52 -0500

    update so it runs properly on the servers

--- src/client/ClientSocketListener.java
+++ src/client/ClientSocketListener.java
@@ -1,10 +1,11 @@
 package client;
+import shared.messages.KVM;
 
 public interface ClientSocketListener {
 
 	public enum SocketStatus{CONNECTED, DISCONNECTED, CONNECTION_LOST};
 	
-	public void handleNewMessage(TextMessage msg);
+	public void handleNewMessage(KVM msg);
 	
 	public void handleStatus(SocketStatus status);
 }
