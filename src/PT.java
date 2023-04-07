80f5894486919e96b3d86548f9e775551af72326
Christian Zeni <chaxzeni@gmail.com>
2023-04-07 00:42:42 +0000

    fixed pt

--- src/PT.java
+++ src/PT.java
@@ -22,57 +22,66 @@ public class PT implements Runnable {
     public double put_time = 0;
     public double get_time = 0;
     public boolean running = true;
-    public PT(int base_port){
+
+    public PT(int base_port) {
         this.base_port = base_port;
     }
 
     public void run() {
-        KVM msg = null; 
+        KVM msg = null;
         double startTime = System.currentTimeMillis();
         double endTime = System.currentTimeMillis();
         Random rand = new Random();
-        try{ 
-            this.kvClient = new KVStore("localhost", 7005);
+        try {
+            this.kvClient = new KVStore("localhost", this.base_port);
             this.kvClient.connect();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
 
-        for(int i =0; i<50; i++){
-            try{
+        for (int i = 0; i < 50; i++) {
+            try {
                 String key = "key" + rand.nextInt(50);
                 startTime = System.currentTimeMillis();
                 kvClient.put(key, "vlad");
                 endTime = System.currentTimeMillis();
-                this.put_time += (endTime - startTime) * (1.0/50.0);
+                this.put_time += (endTime - startTime); //* (1.0 / 50.0);
 
                 key = "key" + rand.nextInt(50);
                 startTime = System.currentTimeMillis();
                 kvClient.get(key);
                 endTime = System.currentTimeMillis();
-                this.get_time += (endTime - startTime) * (1.0/50.0);
+                this.get_time += (endTime - startTime); //* (1.0 / 50.0);
 
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println(e);
             }
         }
-        // this.get_time = this.get_time / 50.0;
-        // this.put_time = this.put_time / 50.0;
+        this.get_time = this.get_time / 50.0;
+        this.put_time = this.put_time / 50.0;
         this.running = false;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int nmb_servers = Integer.parseInt(args[0]);
         int nmb_clients = Integer.parseInt(args[1]);
         int cacheSize = Integer.parseInt(args[2]);
         int base_port = Integer.parseInt(args[3]);
 
         try {
-            new ECSClient(51001);
+            new ECSClient(51001, "", false);
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        try {
+            new ECSClient(51002, "51001", true);;
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
 
         // Create servers
         for (int i = 0; i < nmb_servers; i++) {
@@ -98,21 +107,21 @@ public class PT implements Runnable {
         }
         float avg_get = 0;
         float avg_put = 0;
-        for(PT client: clients){
-            while(client.running){
-                avg_get += client.get_time * (1.0/Double.valueOf(nmb_clients));
-                avg_put += client.put_time * (1.0/Double.valueOf(nmb_clients));
+        for (PT client : clients) {
+            while (client.running) {
+                avg_get += client.get_time * (1.0 / Double.valueOf(nmb_clients));
+                avg_put += client.put_time * (1.0 / Double.valueOf(nmb_clients));
             }
         }
-        try{
+        try {
             String data1 = "Average Get:" + avg_get;
             String data2 = "Average Put:" + avg_put;
             FileWriter writer = new FileWriter("out.txt");
             writer.write(data1);
+            writer.write("\n");
             writer.write(data2);
-            writer.write("tests");
             writer.flush();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e);
         }
         System.out.println("Average Get:" + avg_get);
